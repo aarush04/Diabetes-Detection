47dc9fb6e6d7df8b983af2a673b21c153a14eff5
aarush04 <69864023+aarush04@users.noreply.github.com>
2023-02-09 12:33:55 -0600

    Delete functions.hpp

--- functions.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef FUNCTIONS_HPP
-#define FUNCTIONS_HPP
-#include <string>
-#include <iostream>
-// Your function declarations should go in this header file.
-std::string HawaiinWords(std::string const& str);
-std::string LowerCase(std::string str);
-
-#endif
